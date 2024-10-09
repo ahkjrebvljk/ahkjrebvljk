1bfa98f8205f67cb0eef8e4b3156baab3a7a0022
ahkjrebvljk <kikuma-yukinao-pz@ynu.jp>
2024-10-09 14:50:38 +0900

    Add files via upload

---
+++ J1_1.java
@@ -0,0 +1,14 @@
+
+public class J1_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        int x=25;
+        int y=40;
+        
+        System.out.println("xの値は"+x+"です。");
+        System.out.println("yの値は"+y+"です。");
+        System.out.println("x+yの値は"+(x+y)+"です。");
+	}
+
+}
